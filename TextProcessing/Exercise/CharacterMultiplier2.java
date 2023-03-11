fbfdbb9dac18ce09a7c28bc7c0b9ae4b0c4fce42
Plamen Kamenov <112328223+pako8101@users.noreply.github.com>
2023-03-11 18:18:09 +0200

    Add files via upload

---
+++ TextProcessing/Exercise/CharacterMultiplier2.java
@@ -0,0 +1,10 @@
+package ProgrammingFundamentalsWithJava2023.TextProcessing.Exercise;
+
+import java.util.Scanner;
+
+public class CharacterMultiplier2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
